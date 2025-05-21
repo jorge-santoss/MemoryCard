67bc224beaebb925f0f27065b62a7615125a7f14
Jorge Santos <jorge.santos@laplateforme.io>
2025-05-21 13:45:50 +0200

    Memory card Game

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Cards from './components/Cards';
+function App() {
+  return (
+    <div className="App">
+        <h1>Memory Game</h1>
+        {/* <h3>Welcome to the Memory Game!</h3> */}
+        <h3>Try to match all the pairs of cards!</h3>
+        <Cards />
+        
+    </div>
+  );
+}
+  
+
+export default App;
